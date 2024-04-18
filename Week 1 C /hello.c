2806350961b62f2999b1db9773da5e97928f517b
Abid Hasan Saem <csbyabid@gmail.com>
2024-04-18 16:02:00 +0600

    Updated hello.c

---
+++ Week 1 C /hello.c
@@ -0,0 +1,9 @@
+#include <cs50.h> // adding cs50 library
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What's your name? "); // asks user for name
+
+    printf("Hello, %s\n", name); // prints output hello + user's name
+}
